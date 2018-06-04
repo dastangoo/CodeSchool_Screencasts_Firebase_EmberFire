b433164e76c95f734f0d3ec2e0cf0ccc524e5957
dastangoo <hamed.dastangoo@gmail.com>
2018-06-04 12:36:13 +0430

    Firebase - EmberFire

--- app/app.js
+++ app/app.js
@@ -2,11 +2,16 @@ import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import Firebase from 'emberfire';
 
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
+});
+
+App.ApplicaitonAdapter = DS.FirebaseAdapter.extend({
+  firebase: new Firebase("https://emberfire-project.firebaseio.com")
 });
 
 loadInitializers(App, config.modulePrefix);
--- config/environment.js
+++ config/environment.js
@@ -20,6 +20,13 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    firebase: {
+        apiKey: 'AIzaSyDn30AMrrllc3KsRR8E-G0-5leyFQ_z25g',
+        authDomain: 'emberfire-project.firebaseapp.com',
+        databaseURL: 'https://emberfire-project.firebaseio.com',
+        storageBucket: 'emberfire-project.appspot.com',
     }
   };
 
